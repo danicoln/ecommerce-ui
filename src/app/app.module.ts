4913acc5d44bcccad93fdab5d4d7174e80aaf39f
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-01-31 19:43:12 -0300

    94. Adição de componente Pesquisar no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,11 @@ import { AppComponent } from './app.component';
 import { ProdutoListComponent } from './produto-list/produto-list.component';
 import { ProdutoService } from './services/produto.service';
 import { CategoriaProdutoMenuComponent } from './components/categoria-produto-menu/categoria-produto-menu.component';
+import { PesquisarComponent } from './components/pesquisar/pesquisar.component';
 
 const routes: Routes = [
 
+  {path: 'pesquisar/:keyword', component: ProdutoListComponent},
   {path: 'categoria/:id/:nome', component: ProdutoListComponent},
   {path: 'categoria', component: ProdutoListComponent},
   {path: 'produtos', component: ProdutoListComponent},
@@ -22,6 +24,7 @@ const routes: Routes = [
     AppComponent,
     ProdutoListComponent,
     CategoriaProdutoMenuComponent,
+    PesquisarComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
