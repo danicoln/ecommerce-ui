502fb27b6fa63773bd5a7186b44a3c71a165f4b5
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-02-01 20:40:16 -0300

    121. Criação do componente de status do carrinho

---
+++ src/app/components/status-carrinho/status-carrinho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status-carrinho',
+  templateUrl: './status-carrinho.component.html',
+  styleUrls: ['./status-carrinho.component.css']
+})
+export class StatusCarrinhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
