90c384251e67981ad1365b2479d1d33278bf3b4d
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-02-03 17:01:43 -0300

    Refatorando a view e atualizando o status do carrinho de compras

--- src/app/components/status-carrinho/status-carrinho.component.ts
+++ src/app/components/status-carrinho/status-carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoService } from './../../services/carrinho.service';
 
 @Component({
   selector: 'app-status-carrinho',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatusCarrinhoComponent implements OnInit {
 
-  constructor() { }
+  precoTotal: number = 0.00;
+  quantidadeTotal: number = 0;
+
+  constructor(
+    private carrinhoService: CarrinhoService
+  ) { }
 
   ngOnInit(): void {
+    this.atualizarStatusCarrinho();
+  }
+
+  atualizarStatusCarrinho() {
+
+    //inscrever o preço total do carrinho
+    this.carrinhoService.precoTotal.subscribe(
+      dados => this.precoTotal = dados
+    );
+
+    //inscrever a quantidade total do carrinho
+    this.carrinhoService.quantidadeTotal.subscribe(
+      dados => this.quantidadeTotal = dados
+    );
   }
 
 }
