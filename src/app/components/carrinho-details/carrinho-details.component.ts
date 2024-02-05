76fa1b4eb402fef58bcf9a184a6752cce534fdf3
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-02-05 15:36:45 -0300

    137. Recuperando os dados do carrinho de compras

--- src/app/components/carrinho-details/carrinho-details.component.ts
+++ src/app/components/carrinho-details/carrinho-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemCarrinho } from 'src/app/common/item-carrinho';
+import { CarrinhoService } from 'src/app/services/carrinho.service';
 
 @Component({
   selector: 'app-carrinho-details',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrinhoDetailsComponent implements OnInit {
 
-  constructor() { }
+  itensDoCarrinho: ItemCarrinho[] = [];
+  precoTotal: number = 0;
+  quantidadeTotal: number = 0;
+
+  constructor(private carrinhoService: CarrinhoService) { }
 
   ngOnInit(): void {
+    this.listCarrinhoDetails();
+  }
+
+  listCarrinhoDetails(){
+    //precisamos controlar os itens do carrinho
+    this.itensDoCarrinho = this.carrinhoService.itensCarrinhos;
+
+    //inscrever o preço total
+    this.carrinhoService.precoTotal.subscribe(
+      dados => this.precoTotal = dados
+    );
+
+    //inscrever a quantidade total
+      this.carrinhoService.quantidadeTotal.subscribe(
+        dados => this.quantidadeTotal = dados
+      );
+    // calcular o preço e quantidade total
+    this.carrinhoService.calcularCarrinhoTotal();
   }
 
 }
