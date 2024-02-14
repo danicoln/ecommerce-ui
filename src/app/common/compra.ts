d84a129d4a05b86022a83a6adf148ab3203d05e4
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-02-14 17:14:31 -0300

    211. Implementação das classes common

---
+++ src/app/common/compra.ts
@@ -0,0 +1,13 @@
+import { Cliente } from "./cliente";
+import { Endereco } from "./endereco";
+import { ItemPedido } from "./item-pedido";
+import { Pedido } from "./pedido";
+
+export class Compra {
+
+  cliente: Cliente;
+  enderecoEntrega: Endereco;
+  enderecoCobranca: Endereco;
+  pedido: Pedido;
+  itemPedido: ItemPedido[];
+}
---
+++ src/app/common/item-pedido.ts
@@ -0,0 +1,16 @@
+import { ItemCarrinho } from "./item-carrinho";
+
+export class ItemPedido {
+
+  imageUrl: string;
+  precoUnitario: number;
+  quantidade: number;
+  produtoId: string;
+
+  constructor(itemCarrinho: ItemCarrinho){
+    this.imageUrl = itemCarrinho.imagemUrl;
+    this.quantidade = itemCarrinho.quantidade;
+    this.precoUnitario = itemCarrinho.precoUnitario;
+    this.produtoId = itemCarrinho.id
+  }
+}
