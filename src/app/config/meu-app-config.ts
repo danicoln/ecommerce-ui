48c3471182cf93736727b985e055ec203bbfb875
Daniel Lincoln <lincolndeveloper@outlook.com>
2024-02-16 18:29:52 -0300

    226. Implementando a classe default de configuração

---
+++ src/app/config/meu-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+  openIdConnect: {
+    clientId: '0oaf7b1qmhKZ7bTUQ5d7',
+    issuer: 'https://dev-31835725.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
